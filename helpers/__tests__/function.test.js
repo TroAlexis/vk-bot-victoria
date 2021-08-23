1c8f9903617042d734cfdb9de793bda31ca7bba9
Alex Troshin <a.troshin@idaproject.com>
2021-08-23 19:47:12 +0300

    refactor: refactored test because of the function change

--- helpers/__tests__/function.test.js
+++ helpers/__tests__/function.test.js
@@ -16,14 +16,14 @@ const callTimes = (f, times = props.max + 1) => {
 
 describe('limitCalls', () => {
   test(`func is called ${props.max} times`, () => {
-    const f = createLimitedFunction();
+    const [f] = createLimitedFunction();
     callTimes(f);
     expect(func.mock.calls.length).toBe(5);
   });
 
   test('func is called with correct arguments', () => {
     const args = ['arg1', 'arg2'];
-    const f = createLimitedFunction();
+    const [f] = createLimitedFunction();
     callTimes(() => {
       f(...args);
     });
@@ -31,19 +31,21 @@ describe('limitCalls', () => {
   });
 
   test('callback should be called 1 times', () => {
-    const f = createLimitedFunction();
+    const [f] = createLimitedFunction();
     callTimes(f);
     expect(onLimit).toHaveBeenCalled();
   });
 
   test('resets count on limit reach if resetOnLimitReach is true', () => {
-    const f = createLimitedFunction({
+    const [f, controller] = createLimitedFunction({
       resetOnLimitReach: true,
     });
     callTimes(f, 6);
     expect(func.mock.calls.length).toBe(5);
     expect(onLimit.mock.calls.length).toBe(1);
     callTimes(f, 2);
+    expect(onLimit.mock.calls.length).toBe(1);
     expect(func.mock.calls.length).toBe(7);
+    expect(controller.calls).toBe(2);
   });
 });
