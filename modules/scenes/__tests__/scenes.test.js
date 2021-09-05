34b2c6e08b3a0bea9f71853f8bc66c4fe29299d7
Alex Troshin <a.troshin@idaproject.com>
2021-09-05 14:25:07 +0300

    refactor(modules/scenes): removed complicated by nature test for calling next scene or entering scene

--- modules/scenes/__tests__/scenes.test.js
+++ modules/scenes/__tests__/scenes.test.js
@@ -1,20 +1,8 @@
 const { scenes } = require('~/modules/scenes');
-const { ctxBaseWithScene } = require('~/mocks/contexts');
 
 describe.each(scenes)('$name', ({ name, steps }) => {
   test('contains name and middlewares', () => {
     expect(name).toBeTruthy();
     expect(steps.length).toBeGreaterThan(0);
   });
-  const allStepsExceptLast = steps.slice(0, -1);
-  if (allStepsExceptLast.length) {
-    describe.each(allStepsExceptLast)('each step except for last', (middleware) => {
-      test('proceeds to next scene or enters another scene', async () => {
-        middleware(ctxBaseWithScene);
-        const nextCalled = !!ctxBaseWithScene.scene.next.mock.calls.length;
-        const enterCalled = !!ctxBaseWithScene.scene.enter.mock.calls.length;
-        expect(nextCalled || enterCalled).toBe(true);
-      });
-    });
-  }
 });
